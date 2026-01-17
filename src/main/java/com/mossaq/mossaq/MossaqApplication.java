383fc4154190a57e0aab77f393fd5274f69cbbcf
aalachi <mad.aalachi@gmail.com>
2026-01-17 13:42:27 +0100

    feat: empty skeleton

---
+++ src/main/java/com/mossaq/mossaq/MossaqApplication.java
@@ -0,0 +1,13 @@
+package com.mossaq.mossaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MossaqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MossaqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mossaq/mossaq/MossaqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mossaq.mossaq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MossaqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
